379fe6f74390e15c7d9954549cd393b91dc5262d
michaels <michaels@teoco.com>
2021-04-19 17:15:45 +0300

    first commit

---
+++ hello_there.cxx
@@ -0,0 +1,15 @@
+
+
+#include <iostream>
+//#include <string>
+
+int main() 
+{
+     std::cout << "Hello World!\n";
+     int i = 9;
+     int z = i*i;
+     std::string name = "Bob";
+     std::cout << "Hello there "<<name<<"...\n";
+     std::cout << "z="<<z<<"\n";
+     return 0;
+}
