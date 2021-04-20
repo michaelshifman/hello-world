4afb1f58abfe25f318fb759b972d85e297dd60b9
michaels <michaels@teoco.com>
2021-04-20 16:35:39 +0300

    some message

--- hello_there.cxx
+++ hello_there.cxx
@@ -1,14 +1,14 @@
-
+//github repository
 
 #include <iostream>
 //#include <string>
 
 int main() 
 {
-     std::cout << "Hello World!\n";
+     std::cout << "Hello there World!\n";
      int i = 9;
      int z = i*i;
-     std::string name = "Bob";
+     std::string name = "Bob2";
      std::cout << "Hello there "<<name<<"...\n";
      std::cout << "z="<<z<<"\n";
      return 0;
