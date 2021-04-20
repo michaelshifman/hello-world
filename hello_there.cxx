08f43d2e865b150731c907734976a601ac2066be
michaels <michaels@teoco.com>
2021-04-20 16:55:47 +0300

    ffff

--- hello_there.cxx
+++ hello_there.cxx
@@ -2,10 +2,15 @@
 
 #include <iostream>
 //#include <string>
+using namespace std;
 
 int main() 
 {
      std::cout << "Hello there World!\n";
+     long long n = 2;
+     cout << n;
+     cin >> n;
+     cout << n;
      int i = 9;
      int z = i*i;
      std::string name = "Bob2";
